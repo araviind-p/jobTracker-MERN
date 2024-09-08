efebb82778c29b7ba56ab3d004b2419656c91964
Aravind P <aravindparavind10@gmail.com>
2024-09-08 10:03:44 +0530

    update in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,40 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { setAccessToken, setLoading } from './redux/appSlice';
+import Spinner from './components/Spinner';
 
 function App() {
 
+
+  const { accessToken, loading } = useSelector(store => store.appSlice)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    try {
+      dispatch(setLoading(true))
+      const accessToken = localStorage.getItem("accessToken")
+      dispatch(setAccessToken(accessToken))
+    } catch (err) {
+      console.log(err);
+    } finally {
+      dispatch(setLoading(false))
+    }
+  }, [dispatch])
+  if (loading) {
+    // Show loading spinner while checking accessToken
+    return (
+      <Spinner />
+    );
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/' element={accessToken ? <Profile /> : <Home />} />
+        <Route path='/register' element={accessToken ? <Profile /> : <Register />} />
+        <Route path='/login' element={accessToken ? <Profile /> : <Login />} />
+        <Route path='/profile' element={accessToken ? <Profile /> : <Home />} />
       </Routes>
     </BrowserRouter>
   );

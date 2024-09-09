736624898f641df98e82af4418ed2894168382b1
Aravind P <aravindparavind10@gmail.com>
2024-09-09 16:26:22 +0530

    loading added in login and register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,8 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { setAccessToken } from '../redux/appSlice';
+import { setAccessToken, setLoading } from '../redux/appSlice';
+import Spinner from './Spinner';
 
 function Login() {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ function Login() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const { accessToken } = useSelector(store => store.appSlice)
+  const { accessToken, loading } = useSelector(store => store.appSlice)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -26,7 +27,7 @@ function Login() {
   // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent default form submission
-
+    dispatch(setLoading(true))
     try {
       const response = await axios.post('https://jobtracker-mern-0i5g.onrender.com/api/auth/login', {
         email,
@@ -44,6 +45,8 @@ function Login() {
       // Handle errors here
       setError('Login failed. Please check your credentials and try again.');
       console.error(err);
+    } finally {
+      dispatch(setLoading(false))
     }
   };
 
@@ -55,51 +58,55 @@ function Login() {
 
   return (
     <div className='bg-gray-900 min-h-screen flex justify-center items-center pb-14'>
-      <form className="bg-gray-900 p-6 rounded-lg shadow-lg w-full max-w-sm sm:max-w-md" onSubmit={handleSubmit}>
-        <label className="text-4xl mb-8 font-medium text-gray-900 dark:text-blue-200 flex justify-center">Login</label>
-        {/* {error && <div className="mb-4 text-red-500 text-center">{error}</div>} */}
-        {error && toast.error(error)}
-        <div className="mb-5">
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder='Password'
-            required
-          />
-        </div>
-        <div className='w-full flex justify-center'>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-2/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
-          >
-            Login
-          </button>
-        </div>
-        <div className="text-center">
-          <span className="text-white dark:text-gray-300">Don't have an account?</span>
-          <button
-            type="button"
-            onClick={handleRegisterRedirect}
-            className="ml-2 text-blue-500 hover:underline focus:outline-none"
-          >
-            Register
-          </button>
-        </div>
-      </form>
+      {
+        loading ? (<Spinner />) : (
+          <form className="bg-gray-900 p-6 rounded-lg shadow-lg w-full max-w-sm sm:max-w-md" onSubmit={handleSubmit}>
+            <label className="text-4xl mb-8 font-medium text-gray-900 dark:text-blue-200 flex justify-center">Login</label>
+            {/* {error && <div className="mb-4 text-red-500 text-center">{error}</div>} */}
+            {error && toast.error(error)}
+            <div className="mb-5">
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder='Password'
+                required
+              />
+            </div>
+            <div className='w-full flex justify-center'>
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-2/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
+              >
+                Login
+              </button>
+            </div>
+            <div className="text-center">
+              <span className="text-white dark:text-gray-300">Don't have an account?</span>
+              <button
+                type="button"
+                onClick={handleRegisterRedirect}
+                className="ml-2 text-blue-500 hover:underline focus:outline-none"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        )
+      }
       <ToastContainer />
     </div>
   );
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading } from '../redux/appSlice';
+import Spinner from './Spinner';
 
 function Register() {
   const navigate = useNavigate();
@@ -10,6 +13,9 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+
+  const { loading } = useSelector(store => store.appSlice)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -21,6 +27,7 @@ function Register() {
   // Handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent default form submission
+    dispatch(setLoading(true))
 
     try {
       const response = await axios.post('https://jobtracker-mern-0i5g.onrender.com/api/auth/register', {
@@ -39,6 +46,8 @@ function Register() {
     } catch (err) {
       setError('Registration failed. Please try again.');
       console.error(err);
+    } finally {
+      dispatch(setLoading(false))
     }
   };
 
@@ -49,62 +58,66 @@ function Register() {
 
   return (
     <div className="bg-gray-900 min-h-screen flex justify-center items-center pb-14">
-      <form className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-sm sm:max-w-md" onSubmit={handleSubmit}>
-        <h1 className="text-4xl mb-8 font-medium text-blue-200 flex justify-center">Register</h1>
-        {error && <div className="mb-4 text-red-500 text-center">{error}</div>}
-        
-        <div className="mb-5">
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
-            placeholder="Name"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
-            placeholder="Password"
-            required
-          />
-        </div>
-        <div className="w-full flex justify-center">
-          <button
-            type="submit"
-            className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-2/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
-          >
-            Register
-          </button>
-        </div>
-        <div className="text-center">
-          <span className="text-gray-300">Already have an account?</span>
-          <button
-            type="button"
-            onClick={handleSignInRedirect}
-            className="ml-2 text-blue-500 hover:underline focus:outline-none"
-          >
-            Sign In
-          </button>
-        </div>
-      </form>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <form className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-sm sm:max-w-md" onSubmit={handleSubmit}>
+          <h1 className="text-4xl mb-8 font-medium text-blue-200 flex justify-center">Register</h1>
+          {error && <div className="mb-4 text-red-500 text-center">{error}</div>}
+
+          <div className="mb-5">
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="w-full flex justify-center">
+            <button
+              type="submit"
+              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-2/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
+            >
+              Register
+            </button>
+          </div>
+          <div className="text-center">
+            <span className="text-gray-300">Already have an account?</span>
+            <button
+              type="button"
+              onClick={handleSignInRedirect}
+              className="ml-2 text-blue-500 hover:underline focus:outline-none"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      )}
       <ToastContainer />
     </div>
   );
